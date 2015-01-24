c38978774e189b0aa673f8edc9dff30e81880fd0
akihito.nakano <sora.akatsuki@gmail.com>
2015-01-24 17:58:37 +0900

    Add -p --port option

--- main.go
+++ main.go
@@ -1,18 +1,34 @@
 package main
 
-import "path/filepath"
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/jessevdk/go-flags"
+)
 
 var version = "v0.0.1"
+var opts Options
 
 func main() {
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		os.Exit(1)
+	}
+
 	current, _ := filepath.Abs(".")
 	maildrop := NewMaildrop(current + "/.pound")
 
 	host := "localhost"
-	port := 12345
+	var port int
+	if opts.Port != 0 {
+		port = opts.Port
+	} else {
+		port = 12345
+	}
 	s := NewServer(host, port, maildrop)
 
-	err := s.Run()
+	err = s.Run()
 	if err != nil {
 		panic(err)
 	}
---
+++ options.go
@@ -0,0 +1,5 @@
+package main
+
+type Options struct {
+	Port int `short:"p" long:"port" description:"Run on specific port"`
+}
